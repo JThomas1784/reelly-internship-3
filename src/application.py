0d4664f476b275eabda012946e8a91661e0d30e9
Jamonte T <jamonte.thomas17@gmail.com>
2025-01-31 16:47:41 -0500

    Add headless mode

--- src/application.py
+++ src/application.py
@@ -1,11 +1,16 @@
+import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options  # Import Options
 from pages.login_page import LoginPage
 from pages.settings_page import SettingsPage
-import pytest
 
 @pytest.fixture(scope="function")
 def setup(request):
-    driver = webdriver.Chrome()
+    options = Options()
+    options.headless = True
+    options.add_argument("--window-size=1920,1080")
+
+    driver = webdriver.Chrome(options=options)
     main_page = LoginPage(driver)
     settings_page = SettingsPage(driver)
     driver.get("https://soft.reelly.io/sign-in")
